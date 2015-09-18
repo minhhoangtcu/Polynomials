600baa140a4e36ab41d73bf90ff4cc10217b0d24
minhhoangtcu <minh.hoang@tcu.edu>
2015-09-18 16:15:04 -0500

    Initiate the project with the MVC skeleton

---
+++ src/poly/graphics/PolyController.java
@@ -0,0 +1,8 @@
+package poly.graphics;
+
+public class PolyController {
+	
+	public PolyController(MainMenu mainMenu) {
+		// TODO Auto-generated constructor stub
+	}
+}
---
+++ src/poly/graphics/PolyView.java
@@ -0,0 +1,8 @@
+package poly.graphics;
+
+public class PolyView {
+
+	public PolyView(MainMenu mainMenu) {
+		// TODO Auto-generated constructor stub
+	}
+}
