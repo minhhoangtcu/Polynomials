1eb0506c2ec681650e99566ed32eafc86c962d86
minhhoangtcu <minh.hoang@tcu.edu>
2015-09-18 16:51:36 -0500

    Added Input section to the View

--- src/poly/graphics/PolyView.java
+++ src/poly/graphics/PolyView.java
@@ -6,11 +6,21 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.BoxLayout;
+import javax.swing.SwingConstants;
+import javax.swing.JTextField;
+import java.awt.GridLayout;
+import java.awt.CardLayout;
+import java.awt.Font;
+import java.awt.FlowLayout;
+import javax.swing.JButton;
 
 public class PolyView extends JFrame {
 
 	private JPanel contentPane;
 	private MainMenu main;
+	private JTextField textFieldPolyInput;
 
 	/**
 	 * Create the frame.
@@ -18,11 +28,38 @@ public class PolyView extends JFrame {
 	public PolyView(MainMenu main) {
 		this.main = main; 
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 800, 600);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(new BorderLayout(0, 0));
+		
+		JPanel input = new JPanel();
+		contentPane.add(input, BorderLayout.NORTH);
+		input.setLayout(new BorderLayout(0, 0));
+		
+		JLabel lblInput = new JLabel("INPUT");
+		lblInput.setFont(new Font("Tahoma", Font.PLAIN, 25));
+		lblInput.setHorizontalAlignment(SwingConstants.CENTER);
+		input.add(lblInput, BorderLayout.NORTH);
+		
+		JPanel panelInput = new JPanel();
+		input.add(panelInput, BorderLayout.CENTER);
+		panelInput.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		
+		JLabel lblPleaseInsertThe = new JLabel("Please insert the polynomials in the form of: x^2*y^5*z^10");
+		panelInput.add(lblPleaseInsertThe);
+		lblPleaseInsertThe.setHorizontalAlignment(SwingConstants.CENTER);
+		
+		textFieldPolyInput = new JTextField();
+		panelInput.add(textFieldPolyInput);
+		textFieldPolyInput.setColumns(45);
+		
+		JButton btnSave = new JButton("Save");
+		panelInput.add(btnSave);
+		
+		JPanel output = new JPanel();
+		contentPane.add(output, BorderLayout.SOUTH);
 	}
 
 }
