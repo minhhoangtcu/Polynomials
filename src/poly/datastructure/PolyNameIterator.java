21df78ff6b6208663b74584089cea52af89cb96e
minhhoangtcu <minh.hoang@tcu.edu>
2015-09-29 11:09:42 -0400

    initiate Iterator

---
+++ src/poly/datastructure/PolyNameIterator.java
@@ -0,0 +1,14 @@
+package poly.datastructure;
+
+public class PolyNameIterator {
+	
+	PolyNameNode list;
+	PolyNode firstRightNode;
+	PolyNode lastRightNode;
+	
+	public PolyNameIterator(PolyNameNode list) {
+		this.list = list;
+	}
+	
+
+}
