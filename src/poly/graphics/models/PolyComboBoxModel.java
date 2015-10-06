c6810fc3e77a3886c06ec97ca88664366a2350e6
minhhoangtcu <minh.hoang@tcu.edu>
2015-10-06 11:56:06 -0500

    Completed adding custom data model to ComboBox

--- src/poly/graphics/models/PolyComboBoxModel.java
+++ src/poly/graphics/models/PolyComboBoxModel.java
@@ -1,5 +1,41 @@
 package poly.graphics.models;
+import javax.swing.AbstractListModel;
+import javax.swing.ComboBoxModel;
 
-public class PolyComboBoxModel {
+import poly.graphics.PolyModel;
+
+@SuppressWarnings({ "serial", "rawtypes" })
+public class PolyComboBoxModel extends AbstractListModel implements ComboBoxModel {
+	
+	private PolyModel model;
+	private String selected;
+	
+	public PolyComboBoxModel(PolyModel model) {
+		this.model = model;
+		selected = null;
+	}
+
+	@Override
+	public Object getElementAt(int i) {
+		return model.getName(i);
+	}
+
+	@Override
+	public int getSize() {
+		return model.getSize();
+	}
+
+	@Override
+	public Object getSelectedItem() {
+		return selected;
+	}
+
+	@Override
+	public void setSelectedItem(Object selected) {
+		this.selected = (String) selected; 
+		
+	}
+	
+	
 
 }
