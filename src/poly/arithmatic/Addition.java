e657af2b5637076e356f6a5b88d0a9d9af4dcf02
Minh Hoang TCU <minh.hoang@tcu.edu>
2015-10-05 21:25:45 -0500

    Finished the Adding Method (untested)

---
+++ src/poly/arithmatic/Addition.java
@@ -0,0 +1,34 @@
+package poly.arithmatic;
+
+import poly.datastructure.PolyList;
+import poly.datastructure.PolyNameNode;
+
+public class Addition {
+	
+	/*
+	 * Adding two nodes together and produce a new node.
+	 * If one of the node is empty, return the other one as a resulting node.
+	 */
+	public static PolyNameNode add(PolyNameNode first, PolyNameNode second) {
+		if (first.isEmpty() && second.isEmpty()) return null;
+		else if (first.isEmpty()) return second;
+		else if (second.isEmpty()) return first;
+		else {
+			PolyNameNode merged = mergePoly(first, second);
+			return merged;
+		}
+	}
+
+	/*
+	 * Merge two PolyName nodes together without considering anything
+	 */
+	private static PolyNameNode mergePoly(PolyNameNode p1, PolyNameNode p2) {
+		String newPoly = p1.getPolynomial() + " + " + p2.getPolynomial();
+		String newName = p1.getPolyName() + "+" + p2.getPolyName();
+		PolyList list = new PolyList(); // For the sake of using a method within the poly list
+		PolyNameNode newNameNode = list.createNameNode(newName, newPoly);
+		return newNameNode;
+	}
+	
+	
+}
